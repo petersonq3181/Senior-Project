bad806d822b684073ada92a814ce186fd4c8054f
Quinn Peterson <qap2001@gmail.com>
2024-03-16 14:10:23 -0700

    re-scale tests

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ scaler.fit(train[numeric_cols])
 scaled_train = scaler.transform(train[numeric_cols])
 scaled_test = scaler.transform(test[numeric_cols])
 
+rev_scaled_test = np.array(scaler.inverse_transform(scaled_test))
 
 # ----- data setup 
 def create_sequences(input_data, target_data, seq_len):
@@ -109,22 +110,27 @@ model.eval()
 
 with torch.no_grad():
     test_predictions = model(X_test_tensor)
-    
+
     test_predictions = test_predictions.squeeze(-1)
     
     test_loss = criterion(test_predictions, y_test_tensor)
     print(f'Test Loss: {test_loss.item()}')
 
-test_predictions_np = test_predictions.numpy() * scaler.scale_[1] + scaler.min_[1]
-y_test_np = y_test_tensor.numpy() * scaler.scale_[1] + scaler.min_[1]
+y_test = rev_scaled_test[:, 1]
+
+test_predictions_np = test_predictions.numpy()
+test_predictions_np_reshaped = test_predictions_np.reshape(-1, 1)
+dummy_feature = np.zeros_like(test_predictions_np_reshaped)
+test_predictions_combined = np.hstack((dummy_feature, test_predictions_np_reshaped))
+test_predictions_unscaled_combined = scaler.inverse_transform(test_predictions_combined)
+test_predictions_unscaled = test_predictions_unscaled_combined[:, 1]
 
 # plotting
 plt.figure(figsize=(10, 6))
-plt.plot(y_test_np, label='Actual')
-plt.plot(test_predictions_np, label='Predicted')
+plt.plot(y_test, label='Actual')
+plt.plot(test_predictions_unscaled, label='Predicted')
 plt.title('Test Predictions vs Actual')
-plt.xlabel('Time')
-plt.ylabel('Scaled Value')
+plt.xlabel('Time (Hours)')
+plt.ylabel('Wave Height (Meters)')
 plt.legend()
 plt.show()
-
